9fdda630c7f1151cc56eb1657fb13996b95c4773
Humberto Alcocer <humbertowoody@gmail.com>
2024-02-15 13:44:28 -0600

    feat: visualizaciones en escala de grises

--- visualizaciones.py
+++ visualizaciones.py
@@ -21,14 +21,12 @@ comparison_columns = ['tiempo_kem_promedio', 'tiempo_dss', 'tiempo_mensaje',
                       'cpu_kem', 'cpu_dss', 'cpu_mensaje',
                       'ram_kem', 'ram_dss', 'ram_mensaje']
 
+# Define los patrones de relleno para cada nivel
+hatches = ['/', '\\', '|', '-', '+', 'x', 'o', 'O', '.']
+
 # Convertir 'cantidad_de_bytes_de_envio' a categórico para una mejor representación en la gráfica de barras
 df['cantidad_de_bytes_de_envio'] = pd.Categorical(df['cantidad_de_bytes_de_envio'])
 
-# Convertir 'nivel_dss' a categórico para una mejor representación en la gráfica de barras
-df['nivel_dss'] = pd.Categorical(df['nivel_dss'])
-
-# Convertir 'plataforma' a categórico para una mejor representación en la gráfica de barras
-df['plataforma'] = pd.Categorical(df['plataforma'])
 
 # Gráfica de barras donde 'cantidad_de_bytes_de_envio' es la categoría principal
 for column in comparison_columns:
@@ -44,6 +42,23 @@ for column in comparison_columns:
     plt.savefig(f'graficas/{column}_por_cantidad_bytes.png')
     plt.clf()
 
+# Gráfica de barras donde 'cantidad_de_bytes_de_envio' es la categoría principal, en escala de grises
+for column in comparison_columns:
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='cantidad_de_bytes_de_envio', y=column, hue='nivel_dss', data=df, palette='Greys')
+    plt.title(f'Comparación de {column} por Cantidad de Bytes y Nivel DSS')
+    plt.xlabel('Cantidad de Bytes de Envío')
+    plt.ylabel(column)
+    plt.xticks(rotation=45)
+    plt.legend(title='Nivel DSS')
+
+    # Guardar la gráfica en la carpeta 'graficas'
+    plt.savefig(f'graficas/{column}_por_cantidad_bytes_escalagris.png')
+    plt.clf()
+
+# Convertir 'nivel_dss' a categórico para una mejor representación en la gráfica de barras
+df['nivel_dss'] = pd.Categorical(df['nivel_dss'])
+
 # Gráfica de barras donde 'nivel_dss' es la categoría principal
 for column in comparison_columns:
     plt.figure(figsize=(10, 6))
@@ -58,6 +73,22 @@ for column in comparison_columns:
     plt.savefig(f'graficas/{column}_por_nivel_dss.png')
     plt.clf()
 
+# Gráfica de barras donde 'nivel_dss' es la categoría principal, en escala de grises
+for column in comparison_columns:
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='nivel_dss', y=column, hue='plataforma', data=df, palette='Greys')
+    plt.title(f'Comparación de {column} por Nivel DSS y Plataforma')
+    plt.xlabel('Nivel DSS')
+    plt.ylabel(column)
+    plt.xticks(rotation=45)
+    plt.legend(title='Plataforma')
+
+    # Guardar la gráfica en la carpeta 'graficas'
+    plt.savefig(f'graficas/{column}_por_nivel_dss_escalagris.png')
+    plt.clf()
+
+# Convertir 'plataforma' a categórico para una mejor representación en la gráfica de barras
+df['plataforma'] = pd.Categorical(df['plataforma'])
 
 # Gráfica de barras donde 'plataforma' es la categoría principal
 for column in comparison_columns:
@@ -71,4 +102,18 @@ for column in comparison_columns:
 
     # Guardar la gráfica en la carpeta 'graficas'
     plt.savefig(f'graficas/{column}_por_plataforma.png')
+    plt.clf()
+
+# Gráfica de barras donde 'plataforma' es la categoría principal, en escala de grises
+for column in comparison_columns:
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='plataforma', y=column, hue='nivel_dss', data=df, palette='Greys')
+    plt.title(f'Comparación de {column} por Plataforma y Nivel DSS')
+    plt.xlabel('Plataforma')
+    plt.ylabel(column)
+    plt.xticks(rotation=45)
+    plt.legend(title='Nivel DSS')
+
+    # Guardar la gráfica en la carpeta 'graficas'
+    plt.savefig(f'graficas/{column}_por_plataforma_escalagris.png')
     plt.clf()
